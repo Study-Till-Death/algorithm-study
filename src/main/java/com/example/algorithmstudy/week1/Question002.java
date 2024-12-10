acdf17d84942ed79c41710203bbb98865c1d7db9
권재혁 <yoyo3790@entropykorea.com>
2024-12-10 19:39:22 +0900

    순조롭게 2번 해결

--- src/main/java/com/example/algorithmstudy/week1/Question002.java
+++ src/main/java/com/example/algorithmstudy/week1/Question002.java
@@ -4,6 +4,49 @@ class Question002 {
     public int[] solution(int[][] board, int k){
         int[] answer = new int[2];
 
+        char direction = 'R'; // R right L left D down U up
+        int seconds = 0;
+        int x = 0;
+        int y = 0;
+        while(seconds < k){
+            //시간초 무조건 1초씩 증가
+            seconds++;
+
+            //방향 설정 및 회전 // 방향 회전시에는 1초 추가되므로 + 나 - 진행하지 않음
+            switch(direction){
+                case 'R':
+                    if (y + 1 < board[0].length && board[x][y+1] == 0) {
+                        y++;
+                    } else {
+                        direction = 'D';
+                    }
+                    break;
+                case 'L':
+                    if (y - 1 >= 0 && board[x][y-1] == 0) {
+                        y--;
+                    } else {
+                        direction = 'U';
+                    }
+                    break;
+                case 'D':
+                    if (x + 1 < board.length && board[x+1][y] == 0) {
+                        x++;
+                    } else {
+                        direction = 'L';
+                    }
+                    break;
+                case 'U':
+                    if (x - 1 >= 0 && board[x-1][y] == 0) {
+                        x--;
+                    } else {
+                        direction = 'R';
+                    }
+                    break;
+            }
+        }
+        answer[0] = x;
+        answer[1] = y;
+
         return answer;
     }
 
