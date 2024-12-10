94016e0b2f9613f252bfff2c49adb406f4b59a8a
권재혁 <wowwow6994@gmail.com>
2024-12-10 15:44:25 +0900

    week1 문제1~3 기본코드 세팅

---
+++ src/main/java/com/example/algorithmstudy/week1/Question001.java
@@ -0,0 +1,17 @@
+package com.example.algorithmstudy.week1;
+import java.util.*;
+class Question001 {
+    public char[] solution(int n, int[][] ladder){
+        char[] answer = new char[n];
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Question001 T = new Question001();
+        System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+    }
+}
---
+++ src/main/java/com/example/algorithmstudy/week1/Question002.java
@@ -0,0 +1,33 @@
+package com.example.algorithmstudy.week1;
+import java.util.*;
+class Question002 {
+    public int[] solution(int[][] board, int k){
+        int[] answer = new int[2];
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Question002 T = new Question002();
+        int[][] arr1 = {{0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr1, 10)));
+        int[][] arr2 = {{0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr2, 20)));
+        int[][] arr3 = {{0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr3, 25)));
+
+    }
+}
