38b2cf40dcdec23c0fdc8baab35acceaf1690c89
anjungin <159790447+anjungin@users.noreply.github.com>
2025-01-05 21:45:12 +0900

    2주차 풀이 (#33)
    * 알고리즘 문제4 좌석번호
    
    * 알고리즘 문제5 최대길이 바이토닉 수열
    
    * 알고리즘 문제6 과일 가져가기

---
+++ src/main/java/com/algorithm/a04_sitnumber.java
@@ -0,0 +1,40 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int c, int r, int k) {
+        int[] answer = {1, 1}; // 시작 x,y
+        int[] px = {0, 1, 0, -1}; // 한 바퀴 x증감폭
+        int[] py = {1, 0, -1, 0}; // 한 바퀴 y증감폭
+        int index = 0; // x,y증감폭의 index
+        int cornerHistory = 1; //코너를 돈 횟수
+        int cornerNum = r - 1; //코너 돌 때까지 이동해야하는 횟수
+        int cnt = 0; // 한 코너를 돌 때까지 이동횟수
+
+        for (int i = 1; i < k; i++) {
+            if (cornerNum == 0) return new int[]{0, 0}; // 코너까지의 이동해야하는 횟수가 0인데(자리없음) 사람이 더 존재해서 반복을 돌았을 경우
+            answer[0] += px[index]; //이동
+            answer[1] += py[index]; //이동
+            cnt++; // 한 코너를 돌 때까지 이동 횟수
+
+            if (cnt == cornerNum) { //실제이동횟수와 필요한이동횟수비교
+                cornerHistory++; //코너 돈 횟수
+                if (px[index] != 0) cornerNum = r - cornerHistory / 2; // 코너 돌기까지 필요한 이동거리 갱신
+                else cornerNum = c - cornerHistory / 2;
+                index = (index + 1) % 4; // 코너 돌았으므로 x,y 증감 index 갱신
+                cnt = 0; // 한 코너를 돌 때까지 이동 횟수 초기화
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(Arrays.toString(T.solution(6, 5, 12)));
+        System.out.println(Arrays.toString(T.solution(6, 5, 20)));
+        System.out.println(Arrays.toString(T.solution(6, 5, 30)));
+        System.out.println(Arrays.toString(T.solution(6, 5, 31)));
+    }
+}
---
+++ src/main/java/com/algorithm/a06_takeFruits.java
@@ -0,0 +1,61 @@
+package com.algorithm;
+
+public class a06_takeFruits {
+    // 1부터 n까지 총 n명의 학생 존재
+    // 책상위에는 각 바구니 3개 있고 가장 적은 양을 가진 바구니를 갖는다
+    // 교환티켓 1
+    public int solution(int[][] fruit) {
+        int answer = 0;
+        boolean[] exchangeYn = new boolean[fruit.length]; // 교환여부
+        int[][] minNumIndex = getMinNumIndex(fruit); // 각 배열 가장 작은 숫자와 해당하는 인덱스
+
+        for (int i = 0; i < fruit.length - 1; i++) { // 1번부터 교환 start
+            if (exchangeYn[i]) continue; // 교환된 경우 skip
+            for (int j = i + 1; j < fruit.length; j++) { // i 다음숫자부터 체크
+                if (minNumIndex[i][1] == minNumIndex[j][1]) continue; // 동일 인덱스가 가장 작은 수인경우 skip
+                if (fruit[j][minNumIndex[i][1]] - 1 < minNumIndex[j][0] + 1 // 교환 후 가장 적은 과일 수보다 적어지면 skip
+                        || fruit[i][minNumIndex[j][1]] - 1 < minNumIndex[i][0] + 1) continue;
+                if (exchangeYn[j]) continue; // 교환상대가 이미 교환했을 경우
+
+                exchangeYn[i] = true; // 교환
+                exchangeYn[j] = true;
+                minNumIndex[i][0]++;
+                minNumIndex[j][0]++;
+                break;
+            }
+        }
+
+        for (int[] num : minNumIndex) {
+            answer += num[0];
+        }
+
+        return answer;
+    }
+
+    int[][] getMinNumIndex(int[][] fruit) {
+        int tempMinNum;
+        int tempMinIndex;
+        int[][] minNumIndex = new int[fruit.length][2];
+        for (int i = 0; i < fruit.length; i++) {
+            tempMinNum = fruit[i][0];
+            tempMinIndex = 0;
+            for (int j = 0; j < fruit[i].length; j++) {
+                if (tempMinNum > fruit[i][j]) {
+                    tempMinNum = fruit[i][j];
+                    tempMinIndex = j;
+                }
+            }
+            minNumIndex[i][0] = tempMinNum;
+            minNumIndex[i][1] = tempMinIndex;
+        }
+        return minNumIndex;
+    }
+
+    public static void main(String[] args) {
+        a06_takeFruits T = new a06_takeFruits();
+        System.out.println(T.solution(new int[][]{{10, 20, 30}, {12, 15, 20}, {20, 12, 15}, {15, 20, 10}, {10, 15, 10}}));
+        System.out.println(T.solution(new int[][]{{10, 9, 11}, {15, 20, 25}}));
+        System.out.println(T.solution(new int[][]{{0, 3, 27}, {20, 5, 5}, {19, 5, 6}, {10, 10, 10}, {15, 10, 5}, {3, 7, 20}}));
+        System.out.println(T.solution(new int[][]{{3, 7, 20}, {10, 15, 5}, {19, 5, 6}, {10, 10, 10}, {15, 10, 5}, {3, 7, 20}, {12, 12, 6}, {10, 20, 0}, {5, 10, 15}}));
+    }
+}
