afcb78e7840c3ce2341214b00e74b5fcfbb50762
DGJSKIM <kjsaj0525@gmail.com>
2025-02-03 01:59:39 +0900

    21번 회의실 풀이
    이해완료

--- src/main/java/com/example/algorithmstudy/datastructure/week7/MeetingRoom.java
+++ src/main/java/com/example/algorithmstudy/datastructure/week7/MeetingRoom.java
@@ -28,43 +28,36 @@ public class MeetingRoom {
             int[] meeting = meetingQueue.poll(); // 우선순위 높은 회의 꺼내서
 
             // 일단 그 전에 쓸 수 있는 방 최신화
-            for (Room room : usingRoomQueue) {
-                if (room.time <= meeting[0]) {
-                    availableRoomQueue.add(room);
-                }
+            while (!usingRoomQueue.isEmpty() && usingRoomQueue.peek().time <= meeting[0]) {
+                Room finishedRoom = usingRoomQueue.poll();
+                availableRoomQueue.add(finishedRoom);
             }
 
             if (!availableRoomQueue.isEmpty()) { // 쓸 수 있는 방이 있다면
                 Room room = availableRoomQueue.poll(); // 이용 우선순위 높은 방에 넣을 예정
                 room.count++;
-                room.time = meeting[0] + meeting[1];
-                usingRoomQueue.add(room);
+                room.time =  meeting[1]; // 회의가 끝날 시간 갱신
+                usingRoomQueue.add(room); // 사용중인 방 큐에 넣기
             } else { // 없다면 새 방이 나올때까지 존버
                 Room room = usingRoomQueue.poll(); // 가장 먼저 끝나는 회의실
                 room.count++;
-                room.time += meeting[1];
-                usingRoomQueue.add(room);
+                room.time += meeting[1]-meeting[0]; // 회의가 끝날 시간 갱신
+                usingRoomQueue.add(room); // 사용중인 방 큐에 넣기
             }
         }
 
         // 모든 사용중인 방 종료처리
-        for (Room room : usingRoomQueue) {
-            availableRoomQueue.add(room);
+        while (!usingRoomQueue.isEmpty()) {
+            availableRoomQueue.add(usingRoomQueue.poll());
         }
 
-        int count = 0;
-        // 우선순위 큐는 for문 돌리면 우선순위 순서대로 안나온다함
-//        for (Room room : availableRoomQueue) {
-//            if(count < room.count) {
-//                count = room.count;
-//                answer = room.no;
-//            }
-//        }
-
+        // 회의 진행한 회의실 번호 찾기
+        int maxCount = 0;
+        // 가장 많이 회의가 진행된 방을 찾음
         while (!availableRoomQueue.isEmpty()) {
             Room room = availableRoomQueue.poll();
-            if(count < room.count) {
-                count = room.count;
+            if (room.count > maxCount) {
+                maxCount = room.count;
                 answer = room.no;
             }
         }
@@ -73,9 +66,9 @@ public class MeetingRoom {
     }
 
     private class Room {
-        int no;
-        int count;
-        int time;
+        int no;     // 방 번호
+        int count;  // 해당 방에서 진행한 회의 수
+        int time;   // 마지막 회의 종료 시간
 
         public Room(int no, int count, int time) {
             this.no = no;
@@ -86,9 +79,9 @@ public class MeetingRoom {
 
     public static void main(String[] args) {
         MeetingRoom T = new MeetingRoom();
-        System.out.println(T.solution(2, new int[][]{{0, 5}, {2, 7}, {4, 5}, {7, 10}, {9, 12}}));
-        System.out.println(T.solution(3, new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
-        System.out.println(T.solution(3, new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
-        System.out.println(T.solution(4, new int[][]{{3, 20}, {1, 25}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
+        System.out.println(T.solution(2, new int[][]{{0, 5}, {2, 7}, {4, 5}, {7, 10}, {9, 12}})); // 0
+        System.out.println(T.solution(3, new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}})); // 0
+        System.out.println(T.solution(3, new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}})); // 1
+        System.out.println(T.solution(4, new int[][]{{3, 20}, {1, 25}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}})); // 2
     }
 }
