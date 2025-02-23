233aa8575deef9d836f2a59e8ba5f7af47f4a2da
안정인 <ttingin99@gmail.com>
2025-02-24 08:46:28 +0900

    알고리즘 문제30 이동횟수

---
+++ src/main/java/com/algorithm/a30_move.java
@@ -0,0 +1,28 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class a30_move {
+    public int solution(int[] nums){
+        int answer = 0;
+        int low = 0;
+        int high = nums.length - 1;
+
+        Arrays.sort(nums);
+        while (low <= high) {
+            if (nums[low] + nums[high] <= 5) { // 가장 가벼운 거랑 무거운 거 합이 5보다 작으면
+                low++;  // 가장 가벼운 거도 담음
+            }
+            high--; // 가장 무거운 거를 담음
+            answer++; //이동횟수추가
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        a30_move T = new a30_move();
+        System.out.println(T.solution(new int[]{2, 5, 3, 4, 2, 3}));
+        System.out.println(T.solution(new int[]{2, 3, 4, 5}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3}));
+    }
+}
